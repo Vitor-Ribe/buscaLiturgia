9285352569ffa069349c63926a8c32ddfca25bd9
Vitor Ribeiro <167051938+Vitor-Ribe@users.noreply.github.com>
2024-09-15 20:22:31 -0300

    Update obtemTokenPagina.py

--- obtemTokenPagina.py
+++ obtemTokenPagina.py
@@ -1,8 +1,8 @@
-# Caso você não consiga um token de acesso a página, pode usar esse código para gerar um através de seu token de usuário. Todas os pré-requisitos listados no READ-ME devem ser cumpridos para que esse programa funcione.
+# Caso você não consiga um token de acesso a página, pode usar este código para gerar um através de seu token de usuário. Todas os pré-requisitos listados no READ-ME devem ser cumpridos para que esse código funcione.
 import requests
 
-user_access_token = 'SEU_TOKEN_DE_USUARIO'
-page_id = 'ID_DA_PAGINA'
+user_access_token = 'SEU_TOKEN_DE_USUARIO'  # Substitua pelo seu token de usuário
+page_id = 'ID_DA_PAGINA'  # Substitua pelo ID da página que deseja o token
 
 # URL para obter o token da página
 url = f'https://graph.facebook.com/{page_id}?fields=access_token&access_token={user_access_token}'
